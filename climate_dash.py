983b343161de632a42889e56149af4b95573a621
domoconnor89 <dominicoconnor89@googlemail.com>
2023-10-20 12:26:57 +0200

    update

--- climate_dash.py
+++ climate_dash.py
@@ -6,8 +6,6 @@ import dash_bootstrap_components as dbc
 
 import plotly.express as px
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import seaborn as sns
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
